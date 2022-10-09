2d741800e71398329dff0c5fd1b70e96526cc282
zhaoyuanmeng <1426593075@qq.com>
2022-10-09 21:08:30 +0800

    feat: 添加轮播图

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ const app = createApp(App);
 
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
-
+import Carousel from "v3-carousel";
 app.use(Antd); // 全局挂在antd
 app.use(router); // 挂载路由
 app.use(store); // 挂载pinia
+app.use(Carousel); // 挂载pinia
 app.mount("#app");
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -19,14 +19,31 @@
         <a-breadcrumb-item>List</a-breadcrumb-item>
         <a-breadcrumb-item>App</a-breadcrumb-item>
       </a-breadcrumb>
-      <div class="test">Content</div>
-      <div class="test">Content</div>
-      <div class="test">Content</div>
-      <div class="test">Content</div>
-      <div class="test">Content</div>
-      <div class="test">Content</div>
-      <div class="test">Content</div>
-      <div class="test">Content</div>
+      <div class="test">
+        <Carousel
+          :autoplay="true"
+          :duration="2000"
+          :initIndex="2"
+          :direction="true"
+          directionMode="hover"
+          :directionSize="20"
+          directionColor="skyblue"
+          :indicator="true"
+          indicatorMode="always"
+          indicatorColor="white"
+          indicatorActiveColor="skyblue"
+          @before-moving="beforeMoving"
+          @after-moving="afterMoving"
+        >
+          <CarouselItem
+            v-for="(item, index) in state.data"
+            :key="index"
+            :idx="index"
+          >
+            <img :src="item" />
+          </CarouselItem>
+        </Carousel>
+      </div>
     </a-layout-content>
     <a-layout-footer :style="{ textAlign: 'center' }">
       The government Design ©2022 By 赵元达&何宇
@@ -34,11 +51,31 @@
   </a-layout>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, reactive } from "vue";
+import swiper from "@/components/swipper/swiper.vue";
 export default defineComponent({
+  components: {
+    swiper,
+  },
   setup() {
+    const state = reactive({
+      data: [
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg2.niutuku.com%2Fdesk%2F1208%2F1300%2Fntk-1300-31979.jpg&refer=http%3A%2F%2Fimg2.niutuku.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1667910998&t=c4457da52a74709a44b4c30fedc8ac34",
+        "https://img2.baidu.com/it/u=1814268193,3619863984&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1665421200&t=e27eb9d3e19f9d79609023bcd4fc4505",
+        "https://img2.baidu.com/it/u=1814268193,3619863984&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1665421200&t=e27eb9d3e19f9d79609023bcd4fc4505",
+      ],
+    });
+    function beforeMoving(dir: { index: number; direction: string }) {
+      // console.log("before", dir);
+    }
+    function afterMoving(dir: { index: number; direction: string }) {
+      // console.log("after", dir);
+    }
     return {
       selectedKeys: ref<string[]>(["2"]),
+      beforeMoving,
+      afterMoving,
+      state,
     };
   },
 });
